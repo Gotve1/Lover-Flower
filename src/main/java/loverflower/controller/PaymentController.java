0654b82f51e9c3ba14f8e4158de96d3600c0a30f
Gotve <akselvolkov40@gmail.com>
2025-05-13 03:58:12 -0700

    Add getPaymentById

--- src/main/java/loverflower/controller/PaymentController.java
+++ src/main/java/loverflower/controller/PaymentController.java
@@ -2,6 +2,16 @@ package loverflower.controller;
 
 import loverflower.model.Payment;
 import loverflower.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/payment")
@@ -15,4 +25,11 @@ public class PaymentController {
     public List<Payment> getPayments() {
         return paymentService.getAllPayments();
     }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    @GetMapping("/{id}")
+    public HttpEntity<Payment> getPaymentById(@PathVariable Long id) {
+        return ResponseEntity.ok(paymentService.getPaymentById(id));
+    }
+
 }
