80ecd0a6bc77743be5dbee039d510a9c20b21709
Gotve <akselvolkov40@gmail.com>
2025-05-13 04:08:53 -0700

    Add, create and update

--- src/main/java/loverflower/controller/PaymentController.java
+++ src/main/java/loverflower/controller/PaymentController.java
@@ -1,15 +1,14 @@
 package loverflower.controller;
 
+import loverflower.dto.PaymentDto;
 import loverflower.model.Payment;
+import loverflower.model.Result;
 import loverflower.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,4 +31,15 @@ public class PaymentController {
         return ResponseEntity.ok(paymentService.getPaymentById(id));
     }
 
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    @PostMapping
+    public Result createPayment(@RequestBody PaymentDto paymentDto) {
+        return paymentService.create(paymentDto);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPER_ADMIN')")
+    public Result updatePayment(@PathVariable Long id, @RequestBody PaymentDto paymentDto) {
+        return paymentService.update(id, paymentDto);
+    }
 }
